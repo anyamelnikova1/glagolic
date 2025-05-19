80d54f927ae141457f8fb531278a7dbee0e3870c
anyamelnikova1 <landeoluxxie@gmail.com>
2025-05-20 02:34:41 +0300

    я сейчас кого-нибудь убъю

--- glagproj/glag_to_kir/views.py
+++ glagproj/glag_to_kir/views.py
@@ -2,11 +2,59 @@ from django.shortcuts import render
 from .forms import TextForm
 from asgiref.sync import sync_to_async
 from django.http import HttpRequest
+from typing import Literal
+import re
 
 # Асинхронная обертка для рендеринга
 @sync_to_async
 def render_template(request, template, context):
     return render(request, template, context)
+
+from typing import Literal
+import re
+
+cyr = ('а б в г д е ж ѕ з Ι и к л м н о п р с т ѹ ф х Ѡ ц ч ш щ ъ ы ь ѣ ю ѧ ѩ ѫ ѭ ꙗ')
+glag = ('Ⰰ Ⰱ Ⰲ Ⰳ Ⰴ Ⰵ Ⰶ Ⰷ Ⰸ Ⰺ Ⰻ Ⰽ Ⰾ Ⰿ Ⱀ Ⱁ Ⱂ Ⱃ Ⱄ Ⱅ Ⱆ Ⱇ Ⱈ Ⱉ Ⱌ Ⱍ Ⱎ Ⱏ ⰟⰊ Ⱐ Ⱑ Ⱓ Ⱔ Ⱕ Ⱖ Ⱗ')
+
+def transliterate(text: str, target: Literal['cyr', 'glag']) -> str:
+    cyr_to_glag = {
+        "а": 'Ⰰ', "б": 'Ⰱ', "в": 'Ⰲ', "г": 'Ⰳ', "д": 'Ⰴ', "е": 'Ⰵ', "ж": 'Ⰶ', "ѕ": 'Ⰷ', "з": 'Ⰸ', "Ι": 'Ⰺ',
+        "и": 'Ⰻ', "к": 'Ⰽ', "л": 'Ⰾ', "м": 'Ⰿ', "н": 'Ⱀ', "о": 'Ⱁ', "п": 'Ⱂ', "р": 'Ⱃ', "с": 'Ⱄ', "т": 'Ⱅ',
+        "ѹ": 'Ⱆ', "ф": 'Ⱇ', "х": 'Ⱈ', "Ѡ": 'Ⱉ', "ц": 'Ⱌ', "ч": 'Ⱍ', "ш": 'Ⱎ', "щ": 'Ⱏ', "ъ": 'Ⱏ', "ы": 'ⰟⰊ',
+        "ь": 'Ⱐ', "ѣ": 'Ⱑ', "ю": 'Ⱓ', "ѧ": 'Ⱔ', "ѩ": 'Ⱕ', "ѫ": 'Ⱖ', "ѭ": 'Ⱗ', "ꙗ": 'Ⱑ', " ": ' '
+    }
+
+    glag_to_cyr = {
+        "Ⰰ": 'а', "Ⰱ": 'б', "Ⰲ": 'в', "Ⰳ": 'г', "Ⰴ": 'д', "Ⰵ": 'е', "Ⰶ": 'ж', "Ⰷ": 'ѕ', "Ⰸ": 'з', "Ⰺ": 'Ι',
+        "Ⰻ": 'и', "Ⰽ": 'к', "Ⰾ": 'л', "Ⰿ": 'м', "Ⱀ": 'н', "Ⱁ": 'о', "Ⱂ": 'п', "Ⱃ": 'р', "Ⱄ": 'с', "Ⱅ": 'т',
+        "Ⱆ": 'ѹ', "Ⱇ": 'ф', "Ⱈ": 'х', "Ⱉ": 'Ѡ', "Ⱌ": 'ц', "Ⱍ": 'ч', "Ⱎ": 'ш', "Ⱏ": 'щ', "ⰟⰊ": 'ы',
+        "Ⱐ": 'ь', "Ⱑ": 'ѣ', "Ⱓ": 'ю', "Ⱔ": 'ѧ', "Ⱕ": 'ѩ', "Ⱖ": 'ѫ', "Ⱗ": 'ѭ', " ": ' '
+    }
+
+    word = ' '
+    newtext = (list(text))
+    for letter in newtext:
+        word += letter + ' '
+
+    if target == 'cyr':
+        target_word = ''
+        for letter in word:
+            target_word += glag_to_cyr.get(letter, letter)
+        converteds = target_word.split('   ')
+        final = ' '.join([''.join(converted.split()) for converted in converteds])
+
+        if 'ъΙ' in final:
+            final = final.replace('ъΙ', 'ы')
+
+        final = re.sub(r'\bѣ', 'ꙗ', final)
+        return final
+    elif target == 'glag':
+        target_word = ''
+        for letter in word:
+            target_word += cyr_to_glag.get(letter.lower(), letter)
+        converteds = target_word.split('   ')
+        final = ' '.join([''.join(converted.split()) for converted in converteds])
+        return final
 
 async def homee(request: HttpRequest):
     result = None
@@ -52,5 +100,8 @@ async def process_text(text: str) -> str:
     
     if has_cyr and has_glag:
         return 'ВЗБОЛТАТЬ, НО НЕ СМЕШИВАТЬ (Кириллицу и Глаголицу)'
-    else:
-        return "все норм\n" + text 
+    elif has_cyr:
+        return await sync_to_async(transliterate)(text, 'glag')
+    elif has_glag:
+        return await sync_to_async(transliterate)(text, 'cyr')
+    
